17fe8c31be582d0151fe6758e5bb36403d2c92e8
mohan4 <mohan@afixi.com>
2023-03-29 07:25:07 +0530

    changes

--- module/report.php
+++ module/report.php
@@ -69,7 +69,7 @@ class report extends common {
     function roomdet() {
         $_REQUEST['start_date'] = $sdate = isset($_REQUEST['start_date']) ? $_REQUEST['start_date'] : date("Y-m-01");
         $_REQUEST['end_date'] = $edate = isset($_REQUEST['end_date']) ? $_REQUEST['end_date'] : date("Y-m-d");
-        $sql = "SELECT * FROM {$this->prefix}reservation WHERE (date(est_depature_date) >= '$sdate' AND date(est_depature_date) <= '$edate') AND cancel_date IS NULL ORDER BY date";
+        $sql = "SELECT * FROM {$this->prefix}reservation WHERE (date(create_date) >= '$sdate' AND date(create_date) <= '$edate') AND cancel_date IS NULL ORDER BY date";
         $data = $this->m->sql_getall($sql);
         $this->sm->assign("data", $data);
     }
